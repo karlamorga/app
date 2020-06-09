22e793c33594d70b9f6f7f4f17b0c007a9293ab9
karlamorga <karladanielacog@gmail.com>
2020-06-08 20:49:47 -0600

    changed layout

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-anything
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="oof.css">
+	<style>
+	</style>
+<title>Pomme</title>
+</head>
+<body>
+<h1>This is a page IG </h1>
+</body>
+</html>
